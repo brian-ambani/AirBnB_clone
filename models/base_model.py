ced9b54a01eb1ff0e9e0457ab6583456d22c7c78
brian-ambani <brainambani1@gmail.com>
2023-05-10 12:51:47 +0300

    update the base class

--- models/base_model.py
+++ models/base_model.py
@@ -10,9 +10,10 @@ import datetime
 class BaseModel:
     """ A class that defines attributes and methods """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, id, created_at, updated_at):
         """ A method that initializes all attributes """
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+
