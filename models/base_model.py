13de6f2f7300ef498d2910b75fcc426e7b4af588
brian-ambani <brainambani1@gmail.com>
2023-05-10 13:28:03 +0300

    updated the base class

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,18 @@ import datetime
 class BaseModel:
     """ A class that defines attributes and methods """
 
-    def __init__(self, id, created_at, updated_at):
-        """ A method that initializes all attributes """
+    def __init__(self, *args, **kwargs):
+        """ A method that initializes attributes
+            Arguements:
+                *args - list of arguements
+                **kwargs - A dict of value with keys
+        """
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
+    def __str__(self):
+        """ Returns class name, id and dict"""
