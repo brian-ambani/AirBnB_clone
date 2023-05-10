3103561ee358977f78b95637401521b289e057b7
brian-ambani <brainambani1@gmail.com>
2023-05-11 02:08:43 +0300

    updated the file_storage file

---
+++ models/engine/file_storage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module containing a class that serializes instances to a JSON file and deserilizes JSON file to instances
+"""
+
+
+class FileStorage:
+    """serilizes instances and deserilizes JSON files"""
+    def __init__(self):
