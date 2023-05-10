4b15b68f519c2e4d5985239040ddd1c7a8c643ab
brian-ambani <brainambani1@gmail.com>
2023-05-10 13:40:43 +0300

    updated the base class

--- models/base_model.py
+++ models/base_model.py
@@ -23,6 +23,16 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
+        else:
+            for key in kwargs:
+                if key == "create_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                            kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
 
     def __str__(self):
         """ Returns class name, id and dict"""
