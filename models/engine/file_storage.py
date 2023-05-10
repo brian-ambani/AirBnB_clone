f6c4ac2eca0022fbf2e57e931c1e8ee15617f20b
brian-ambani <brainambani1@gmail.com>
2023-05-11 02:46:17 +0300

    updated the storage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Module containing a class that serializes instances to a JSON file and deserilizes JSON file to instances
 """
+
 import json
 
 
@@ -8,3 +9,29 @@ class FileStorage:
     """serilizes instances and deserilizes JSON files"""
 
     __file_path = "file.json"
+    __object = {}
+
+    def all(self):
+        """ A method returning dict"""
+
+        return self.objects
+
+    def new(self, obj):
+        """Method that sets in __objects the obj
+        with key <obj class name>.id"""
+
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """serilizes __objects to JSON file & path:
+        __file_path"""
+
+        with open(self.__file_path, 'w') as f:
+            obj_dict = {}
+            for key, value in self.__objects.items():
+                obj_dict[key] = value.to_dict()
+            json.dump(obj_dict, f)
+
+
+
