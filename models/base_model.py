ae5bca5ac39ab63dc763712f8a737e650cd50ac5
brian-ambani <brainambani1@gmail.com>
2023-05-10 14:17:50 +0300

    updated the base class

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             for key in kwargs:
                 if key == "create_at":
@@ -45,7 +44,6 @@ class BaseModel:
         """
 
         self.update_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         """creates a dict with keys and
